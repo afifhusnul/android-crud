cca6bd1a3ab34fd9f044c124b3c20e38e464bbcf
afifhusnul <afif.husnul@gmail.com>
2018-09-04 14:56:07 +0800

    Modif CustomAuthenticator

--- app/src/main/java/test1/android/com/test1/Rest/CustomAuthenticator.java
+++ app/src/main/java/test1/android/com/test1/Rest/CustomAuthenticator.java
@@ -16,29 +16,4 @@ public class CustomAuthenticator implements Authenticator {
     public Request authenticate(Route route, okhttp3.Response response) throws IOException {
         return null;
     }
-    //    @Override
-//    public Request authenticate(Route route, Response response) throws IOException {
-//        try {
-//            if (responseCount(response) > 3) {
-//                return null;
-//            }
-//            /*silahkan diterapkan request api refresh token secara synchronous pada tahap ini*/
-//
-//
-//        } catch (Exception e) {
-//        }
-//
-//        return  response.request()
-//                .newBuilder()
-//                .removeHeader("Authorization")
-//                .addHeader("Authorization", "Bearer " + newToken)
-//                .build();
-//    }
-//    private int responseCount(Response response) {
-//        int result = 1;
-//        while ((response = response.priorResponse()) != null) {
-//            result++;
-//        }
-//        return result;
-//    }
 }
